3c46c5bc80029605713c7765d554538e2ad8638d
Edy Mariano <edy.mariano1@gmail.com>
2022-10-10 10:53:45 +0200

    test

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ robot1 = dashboard_client.DashboardClient("172.31.0.101")
 
 robot1.connect()
 print(robot1.isConnected())
-robot1.loadURP("moveBToC.urp")
+robot1.loadURP("moveBToD.urp")
 robot1.play()
+while True:
+    print(robot1.running())
 
