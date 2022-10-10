335d16837d59d5c4cfa3caeca55ebb50064841bb
Edy Mariano <edy.mariano1@gmail.com>
2022-10-10 11:20:19 +0200

    test

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import time
 import dashboard_client
+import rtde_receive
 
-robot1 = dashboard_client.DashboardClient("172.31.0.101")
+ipAddress = "172.31.0.101"
+
+robot1 = dashboard_client.DashboardClient(ipAddress)
+robot1_receive = rtde_receive.RTDEReceiveInterface(ipAddress)
 
 robot1.connect()
 print(robot1.isConnected())
@@ -10,5 +14,10 @@ robot1.play()
 print("Program is running")
 time.sleep(1)
 while robot1.running():
-    pass
+    if robot1_receive.getDigitalOutState(16):
+        print("Tool digital out (16) is HIGH")
+    else:
+        print("Tool digital out (16) is LOW")
+
+    time.sleep(0.5)
 print("Program is finished")
