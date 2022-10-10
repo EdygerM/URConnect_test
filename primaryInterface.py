23800eaebd1da0135ae305db714c2adc63bfdbc7
Edy Mariano <edy.mariano1@gmail.com>
2022-10-10 15:50:25 +0200

    test

--- primaryInterface.py
+++ primaryInterface.py
@@ -48,7 +48,7 @@ while True:
                 offset += 1
                 popupMessageTitle = struct.unpack_from('!%ds' % popupMessageTitleSize, data, offset)
                 offset += popupMessageTitleSize
-                print(popupMessageTitle.decode())
+                print(popupMessageTitle)
                 popupTextMessage = struct.unpack_from('!s', data, offset)
                 print(popupTextMessage.decode())
 
