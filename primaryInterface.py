4c2de8b96fb7dbf26753da2a7204d7da460e9e9c
Edy Mariano <edy.mariano1@gmail.com>
2022-10-10 14:21:22 +0200

    test

---
+++ primaryInterface.py
@@ -0,0 +1,48 @@
+import socket, struct
+
+HOST = '172.31.0.101'
+PORT = 30002
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.connect((HOST, PORT))
+while True:
+    data = s.recv(4096)
+    offset = 0
+    if data:
+        messageSize = struct.unpack_from('!i', data)[0]
+        offset += 4
+        messageType = struct.unpack_from('!B', data, offset)[0]
+        offset += 1
+        if messageType == 20:
+            timestamp = struct.unpack_from('!Q', data, offset)[0]
+            offset += 8
+            source = struct.unpack_from('!c', data, offset)[0]
+            offset += 1
+            robotMessageType = struct.unpack_from('!c', data, offset)[0]
+            offset += 1
+            if robotMessageType == 2:
+                requestId = struct.unpack_from('!I', data, offset)[0]
+                offset += 4
+                requestedType = struct.unpack_from('!I', data, offset)[0]
+                offset += 4
+                warning = struct.unpack_from('!?', data, offset)[0]
+                offset += 1
+                error = struct.unpack_from('!?', data, offset)[0]
+                offset += 1
+                blocking = struct.unpack_from('!?', data, offset)[0]
+                offset += 1
+                popupMessageTitleSize = struct.unpack_from('!B', data, offset)[0]
+                offset += 1
+                popupMessageTitle = struct.unpack_from('!s', data, offset)[0]
+                offset += popupMessageTitleSize
+                popupTextMessage = struct.unpack_from('!s', data, offset)[0]
+                print(popupTextMessage)
+
+#packageSize = struct.unpack_from('!i', data, 5)[0]
+#packageType = struct.unpack_from('!B', data, 9)[0]
+#if packageType == :
+
+
+
+
