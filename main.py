496227c6d20759a6e4a85838aa8df56dd8a022e7
Edy Mariano <edy.mariano1@gmail.com>
2022-10-10 10:58:35 +0200

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ robot1.connect()
 print(robot1.isConnected())
 robot1.loadURP("moveBToC.urp")
 robot1.play()
-print("Program is running")
+
 while robot1.running():
-    pass
+    print("Program is running")
 print("Program is finished")
